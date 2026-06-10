cb0114366add7c5c4141f68a254915ee9b92cb13
MitPrajapati <pmit9810@gmail.com>
2026-06-10 17:58:35 +0530

    Added edit and delete  functionality on Note page

--- app/__init__.py
+++ app/__init__.py
@@ -16,5 +16,4 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(notes_bp)
 
-
     return app
--- routes/notes.py
+++ routes/notes.py
@@ -25,3 +25,25 @@ def view_notes():
         return redirect(url_for('auth.login'))
     notes = Note.query.filter_by(user_id = session['user_id']).all()
     return render_template('view_notes.html',notes = notes)
+
+@notes_bp.route('/edit/<int:note_id>',methods=['GET'])
+def edit(note_id):
+    note = Note.query.get(note_id)
+    return render_template('edit.html', note=note)
+
+@notes_bp.route('/update/<int:note_id>',methods = ['POST'])
+def update(note_id):
+    note = Note.query.get(note_id)
+    note.title = request.form['title']
+    note.content = request.form['content']
+
+    db.session.commit()
+    return redirect(url_for('notes.view_notes'))
+
+@notes_bp.route('/delete/<int:note_id>',methods=['POST'])
+def delete(note_id):
+    note = Note.query.get(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('notes.view_notes'))
+    
